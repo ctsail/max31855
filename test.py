1e4480ce4ec138a952f647b2f93d2913c6c5c6b9
Adam Ross <3512798+ctsail@users.noreply.github.com>
2019-09-26 04:16:39 +0100

    revise data log filename to be YYYYMMDDHHMMSS.csv add log folder

--- test.py
+++ test.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python
 from max31855 import MAX31855, MAX31855Error
-import time
+import time, datetime
 
 cs_pins = [25]
 clock_pin = 23
@@ -9,8 +9,12 @@ units = "f"
 thermocouples = []
 for cs_pin in cs_pins:
     thermocouples.append(MAX31855(cs_pin, clock_pin, data_pin, units))
+
+date=datetime.datetime.now()
+file="/home/pi/Projects/max31855_logger/logs/{0}.csv".format(date.strftime("%Y%m%d%H%M%S"))
+
 running = True
-with open("/home/pi/Projects/max31855_logger/tmp.csv","a") as log:
+with open(file,"a") as log:
     while(running):
         try:
             for thermocouple in thermocouples:
